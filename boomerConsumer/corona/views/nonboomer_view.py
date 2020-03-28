de7b51cdcb59072fe7a4d2f0996cea0556cd5f30
mzhang <muen2001@hotmail.com>
2020-03-28 17:06:27 -0400

    here

--- boomerConsumer/corona/views/nonboomer_view.py
+++ boomerConsumer/corona/views/nonboomer_view.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.template import loader, RequestContext
 from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
-from ..models import Boomer, Requests
+from ..models import Boomer, Requests, Zoomer
 from django.urls import reverse
 from django.views import generic
 from django.db import connection
@@ -17,8 +17,12 @@ def show_requests(request):
         return render(request, 'nonboomer/taken_requests.html', context)
     elif(request.session['type'] == 'boomer'):
         request_list=Requests.objects.filter(boomer_id=request.session['username'])
+        myrequest_list = []
+        for request in request_list:
+            zoomer = Zoomer.objects.get(pk=request.zoomer_id)
+            myrequest_list.append({'id':request.id, 'boomer_id': request.boomer_id, 'zoomer_id':request.zoomer_id, 'details':request.details,'completed':request.completed,'taken':request.taken,'zoomer':zoomer})
         context = {
-            'request_list':request_list,
+            'request_list':myrequest_list,
         }
         return render(request, 'nonboomer/myrequests.html', context)
     return render(request, 'login/index.html', {})
