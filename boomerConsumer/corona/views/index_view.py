2fbb2b7c28a9130e01dd92769d442b7c2958de67
Gursimmer <46874155+Gurs-Ban@users.noreply.github.com>
2020-03-28 18:49:25 -0400

    form for coomer

--- boomerConsumer/corona/views/index_view.py
+++ boomerConsumer/corona/views/index_view.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.template import loader, RequestContext
 from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
 from django.db import connection
+from django.contrib.auth import authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login as loginDjango
 import datetime
 
 def index(request):
@@ -14,7 +17,26 @@ def login(request):
     return render(request, 'login/index.html',{})
 
 def signupB(request):
-    return render(request, 'login/boomer_signup.html',{})
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            loginDjango(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'login/boomer_signup.html',{'form': form})
 
 def signupZ(request):
     return render(request, 'login/zoomer_signup.html',{})
+
+
+
+    # Boomer.object.filter(pk="whatever they enter")
+
+    # def signup(request):
+    
+    # return render(request, 'signup.html', {'form': form})
